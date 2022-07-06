d04392676cb5dd43007c3af993e802834e610ace
Miguel Nascimento <shimiguelns@gmail.com>
2022-07-06 14:49:27 -0300

    Create main.py

---
+++ testBranch/main.py
@@ -0,0 +1,7 @@
+n1 = input("Você é mimada? s/n ").lower().strip()
+if n1 == "s" or n1 == "sim":
+    print("UUUUIIIII!")
+    print("ELE GOOOOOSTA...")
+else:
+    print("https://www.youtube.com/watch?v=4jv2v9kwh4Q")
+    print("Sem cultura!")
