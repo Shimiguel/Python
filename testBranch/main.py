ea0498cea390dcf2bcb72f48d4d1acd3d79c0e75
Miguel Nascimento <shimiguelns@gmail.com>
2022-07-06 14:51:58 -0300

    Update main.py

--- testBranch/main.py
+++ testBranch/main.py
@@ -1,7 +1,9 @@
 n1 = input("Você é mimada? s/n ").lower().strip()
 if n1 == "s" or n1 == "sim":
+    print("hmmm... me apaixonei...")
     print("UUUUIIIII!")
     print("ELE GOOOOOSTA...")
+    print("TOMI")
 else:
     print("https://www.youtube.com/watch?v=4jv2v9kwh4Q")
     print("Sem cultura!")
