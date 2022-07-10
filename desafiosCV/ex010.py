1ae6a0a9754657ef30d5a004f4631236579b0cb9
Miguel Nascimento <shimiguelns@gmail.com>
2022-07-10 19:25:15 -0300

    Update ex010.py

--- desafiosCV/ex010.py
+++ desafiosCV/ex010.py
@@ -1,3 +1,5 @@
 qt_dinheiro = float(input("Quanto dinheiro gostaria de converter? R$"))
 vl_dollar = 5.26
-print(f"{qt_dinheiro} é igual a {qt_dinheiro / vl_dollar:.2f}.")
+print(f"{qt_dinheiro} é igual a US${qt_dinheiro / vl_dollar:.2f}.")
+vl_iene = 0.039
+print(f"{qt_dinheiro} é igual a ¥{qt_dinheiro / vl_iene:.2f}.")
